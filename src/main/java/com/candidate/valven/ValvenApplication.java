1123db55c449d31b721b26502917bbe65b85d8fe
BerkTorun <berktorun.dev@gmail.com>
2023-10-13 21:34:24 +0300

    refactoring about build

--- src/main/java/com/candidate/valven/ValvenApplication.java
+++ src/main/java/com/candidate/valven/ValvenApplication.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 
 import org.gitlab4j.api.GitLabApiException;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.candidate.valven.service.GitHubService;
